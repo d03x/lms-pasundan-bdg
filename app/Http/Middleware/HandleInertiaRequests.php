a2aa06490603d8e197e5af916914cf5590e02156
d03x <dadanhidyt@gmail.com>
2025-11-06 15:05:33 +0700

    Remove AuthRoleMiddleware and refactor HandleInertiaRequests to utilize AuthServiceInterface for role management

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\Contracts\AuthServiceInterface;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
@@ -36,13 +37,12 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
-
+        $authService = app(AuthServiceInterface::class);
         return [
             ...parent::share($request),
             'name' => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
+                'role'=>$authService->test(),
                 'user' => $request->user(),
             ],
         ];
