d833eae1119ced752a93a8ffd9d9fbc1db673a7f
d03x <dadanhidyt@gmail.com>
2025-11-21 08:55:24 +0700

    Enhance button component to accept additional class props for better styling flexibility

--- resources/js/components/button.vue
+++ resources/js/components/button.vue
@@ -1,12 +1,15 @@
 <template>
-    <button :class="classsess">
-        <slot/>
+    <button :class="cn(classsess, props.class)">
+        <slot />
     </button>
 </template>
 <script setup lang="ts">
+import { cn } from '@/lib/utils';
 import { computed } from 'vue';
-
-const classsess = computed(()=>{
+const props = defineProps<{
+    class?: string;
+}>();
+const classsess = computed(() => {
     return `mt-2 w-full disabled:bg-neutral-500  cursor-pointer rounded-input bg-primary px-3 py-3 text-xs font-semibold text-white`;
-})
+});
 </script>
